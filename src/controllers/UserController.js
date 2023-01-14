c70ba3713c8a265867189a1843d64dff7b91d823
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-14 14:40:45 -0300

    Add Query Params

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,17 @@ const users = require("../mocks/users");
 
 module.exports = {
   listUsers(request, response) {
+    const { order } = request.query;
+    
+    const sortedUsers = users.sort((a, b) => {
+      if (order === "desc") {
+        return a.id < b.id ? 1 : -1;
+      }
+
+      return a.id > b.id ? 1 : -1;
+    });
+
     response.writeHead(200, { "Content-Type": "text/json" });
-    response.end(JSON.stringify(users));
+    response.end(JSON.stringify(sortedUsers));
   }
 };

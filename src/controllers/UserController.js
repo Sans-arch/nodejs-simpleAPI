f7485eba840256c40ee4708192b6af17cbe6a37d
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-14 19:50:53 -0300

    Add delete and update methods to the API

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,5 @@
 // Um controlador é o responsável por armazenar toda a regra de negócio da nossa aplicação
-const bodyParser = require("../helpers/bodyParser");
-const users = require("../mocks/users");
+let users = require("../mocks/users");
 
 module.exports = {
   listUsers(request, response) {
@@ -29,7 +28,7 @@ module.exports = {
   },
   createUser(request, response) {
     const { body } = request;
-    
+
     const lastUserId = users[users.length - 1].id;
     const newUser = {
       id: lastUserId + 1,
@@ -38,5 +37,38 @@ module.exports = {
     users.push(newUser);
 
     response.send(200, newUser);
+  },
+  updateUser(request, response) {
+    let { id } = request.params;
+    const { name } = request.body;
+    
+    id = Number(id);
+
+    const userExists = users.find(user => user.id === id);
+
+    if (!userExists) {
+      return response.send(400, { error: "User not found" });
+    }
+
+    users = users.map(user => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name
+        };
+      }
+
+      return user;
+    });
+
+    return response.send(200, { id, name });
+  },
+  deleteUser(request, response) {
+    let { id } = request.params;
+    id = Number(id);
+
+    users = users.filter(user => user.id !== id);
+
+    return response.send(201, { deleted: true });
   }
 };
